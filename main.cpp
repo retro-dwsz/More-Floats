ad7b6aa3f413c00907b444d59dd185f7a3dc6455
Rétro Dvořák <davidsirait1156@gmail.com>
2026-04-14 20:28:56 +0700

    Init commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <half.hpp>
+
+#include <fmt/format.h>
+#include <fmt/ranges.h>
+#include <fmt/std.h>
+#include <fmt/os.h>
+
+/* Mari kita membuat f8, uf8, f16, dan uf16, yaitu 8-bit & 16-bit floating point, signed & unsigned  */
+
+int main() {
+    return 0;
+}
